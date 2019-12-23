a27e695c08b54af2b9b25e4b2b0d125ca5405ac9
Vuong Cuong <vuongcuong.phoenix@gmail.com>
2019-12-23 14:29:24 +0700

    Seperate into 'framework.sh' and 'post_install_zsh.sh'

--- framework.sh
+++ framework.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-tabs 4
-
-
-
-# @author: vuong-cuong-phoenix
-# @email: vuongcuong.phoenix@gmail.com
-# @github: https://github.com/vuong-cuong-phoenix/
-
-# This is the ultimate dotfiles setup for Linux distributions.
-# Tested on:
-#           - Arch Linux
-
 
 declare -a ALL_PM=(
     # deb
@@ -51,25 +39,10 @@ CURRENT_TIME=$(date +"%F")
 mkdir -p "$MAIN_DIR/BACKUP/$CURRENT_TIME" > /dev/null 2>&1
 BACKUP_DIR="$MAIN_DIR/BACKUP/$CURRENT_TIME"
 
-SYSTEM_FONTS_DIR="/usr/share/fonts"
-
-# if ! command -v "git" > /dev/null 2>&1; then
-#     echo "Error: Git is not installed!"
-#     echo "Need to install Git first!"
-#     exit 1
-# fi
-
-# if [ -d "$DOTFILES_DIR/.git"]; then
-#     cd "$DOTFILES_DIR" || exit
-#     echo "Upgrading "
-
-
-
 #--                                     ULTILIES
 NUM_COLUMNS=$(( $( tput cols ) ))
 SEPERATED_BAR=$(printf "%*s" "$NUM_COLUMNS" "" | tr " " "+")
 PROGRESS_BAR=$(printf "%*s" "$NUM_COLUMNS" "" | tr " " ".")
-
 #--                                     FUNCTIONS
 ################################################################################################
 #                                   execute_quietly() $1
@@ -255,7 +228,6 @@ check_and_download() {
             print_wtabs 2 "Installing '$1'" "SUCCEED"
         else
             print_wtabs 2 "Installing '$1'" "FAILED"
-
             return 1
         fi
     fi
@@ -328,89 +300,3 @@ install_and_config() {
         return 1
     fi
 }
-#################################################################################################                                       install_and_config $1 $2 $3 $4 ...
-#                                   post_install_zsh
-# Desc: Install and configure extras for 'zsh' after it is installed and configured.
-# Arguments:    None
-post_install_zsh() {
-    printf "Configuring extras for 'zsh'\n"
-    # Set 'zsh' as default SHELL
-    if [ ! "$SHELL" = "$(command -v zsh)" ]; then
-        chsh -s $(command -v zsh)
-    fi
-
-    # Configure 'oh-my-zsh'
-    printf "\t'oh-my-zsh'\n"
-    check_and_download "oh-my-zsh" "$HOME/.oh-my-zsh" "sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)""
-    if [ $? -eq 1 ]; then
-        return 1
-    fi
-
-    printf "\t'zsh-autosuggestions' for 'oh-my-zsh'\n"
-    check_and_download "zsh-autosuggestions" "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" "git clone "https://github.com/zsh-users/zsh-autosuggestions" "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions""
-    if [ $? -eq 1 ]; then
-        return 1
-    fi 
-
-    printf "\t'zsh-syntax-highlighting' for 'oh-my-zsh'\n"
-    check_and_download "zsh-syntax-highlighting" "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" "git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting""
-    if [ $? -eq 1 ]; then
-        return 1
-    fi
-    
-    printf "\t'powerlevel10k' for 'oh-my-zsh'\n"
-    # Install 'Powerlevel10k' for 'oh-my-zsh'
-    if [ -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
-        print_wtabs 2 "FOUND 'powerlevel10k'"
-    else
-        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
-        
-        # Check again if 'git clone' goes wrong
-        if [ -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
-            print_wtabs 2 "Installing 'powerlevel10k'" "SUCCEED"
-        else
-            print_wtabs 2 "Installing 'powerlevel10k'" "FAILED"
-            return 1
-        fi
-    fi
-
-    printf "Successfully installed and configured 'oh-my-zsh' and 'powerlevel10k' theme for 'zsh'.\n"
-    printf "To get the best experience on 'zsh', you should install 'Hack Nerd Font' and change Terminal's theme to 'Breeze':\n"
-    printf "\tHack Nerd Font: https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Hack.zip\n"
-
-    return 0
-}
-#--                                     MAIN
-# Install 'git'
-install_and_config git git ".gitconfig.static"
- 
-# Configure .gitconfig
-execute_quietly "command -v git"
-if [ $? -eq 0 ]; then
-    git config --global include.path "$HOME/.gitconfig.static"
-fi
-
-echo $SEPERATED_BAR
-# Install 'cURL'
-install_and_config "" curl
-
-echo $SEPERATED_BAR
-# Install 'unzip'
-install_and_config "" unzip
-
-echo $SEPERATED_BAR
-# Install and configure 'bash' shell
-install_and_config shell bash .bash_profile .bashrc
-
-echo $SEPERATED_BAR
-# Install and configure 'vim' editor
-install_and_config editor vim .vimrc
-
-echo $SEPERATED_BAR
-# Install and configure 'zsh' shell
-install_and_config shell zsh .zprofile .zshrc
-if [ $? -eq 0 ]; then
-    post_install_zsh
-fi
-
-echo $SEPERATED_BAR
---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+source "$HOME/.dotfiles/framework.sh"
+
+tabs 4
+
+
+# @author: vuong-cuong-phoenix
+# @email: vuongcuong.phoenix@gmail.com
+# @github: https://github.com/vuong-cuong-phoenix/
+
+# This is the main setup for Linux distributions.
+# Tested on:
+#           - Arch Linux
+#           - MX Linux
+
+
+
+#--                                     MAIN
+# Install 'git'
+install_and_config git git ".gitconfig.static"
+ 
+# Configure .gitconfig
+execute_quietly "command -v git"
+if [ $? -eq 0 ]; then
+    git config --global include.path "$HOME/.gitconfig.static"
+fi
+
+echo $SEPERATED_BAR
+# Install 'cURL'
+install_and_config "" curl
+
+echo $SEPERATED_BAR
+# Install 'unzip'
+install_and_config "" unzip
+
+echo $SEPERATED_BAR
+# Install and configure 'bash' shell
+install_and_config shell bash .bash_profile .bashrc
+
+echo $SEPERATED_BAR
+# Install and configure 'vim' editor
+install_and_config editor vim .vimrc
+
+echo $SEPERATED_BAR
+# Install and configure 'zsh' shell
+install_and_config shell zsh .zprofile .zshrc
+if [ $? -eq 0 ]; then
+    zsh post_install_zsh.sh
+fi
+
+echo $SEPERATED_BAR
+
+printf "Successfully installed and configured all dotfiles avaiable!\n"
