3c04268059d503ef4e22d73dcb3a334f9a5be0c7
Vuong Cuong <vuongcuong.phoenix@gmail.com>
2020-06-17 02:07:38 +0700

    refactor(install): adapt to new framework & clean-up.

--- install.sh
+++ install.sh
@@ -38,9 +38,9 @@ source "${MAIN_DIR}/framework.sh"
 
 #---------------------------------------- MAIN ----------------------------------------
 # Install 'git'
-install_and_config git git ".gitignore_global"
+install_and_config "git" git git ".gitignore_global"
  
-# Configure .gitconfig
+# Configure '.gitconfig'
 execute_quietly "command -v git"
 if [ $? -eq 0 ]; then
     git config --global color.diff auto
@@ -56,116 +56,79 @@ echo $SEPERATED_BAR
 
 ################################################################
 # Install 'cURL'
-install_and_config "" curl
+install_and_config "" curl curl
 
 echo $SEPERATED_BAR
 
 ################################################################
 # Install 'wget'
-install_and_config "" wget
+install_and_config "" wget wget
 
 echo $SEPERATED_BAR
 
 ################################################################
 # Install 'unzip'
-install_and_config "" unzip
+install_and_config "" unzip unzip
 
 echo $SEPERATED_BAR
 
 ################################################################
 # Install 'fzf'
-install_and_config "" fzf
+install_and_config "" fzf fzf
 
 echo $SEPERATED_BAR
 
 ################################################################
 # Install and config 'ripgrep'
-if ! command -v "rg" > /dev/null 2>&1; then
-    printf "${BOLD}${YELLOW}ripgrep ${NORMAL}${YELLOW}is not installed => "
-    printf "${NORMAL}"
-
-    ask_for_confirmation "${YELLOW}Would you like to install ${BOLD}${YELLOW}ripgrep ${NORMAL}${YELLOW}now?" "y" "yes" "n" "no"
-
-    if [ $? -eq 0 ]; then
-        package_install "ripgrep"
-
-        if [ $? -eq 0 ]; then
-            print_wtabs 2 "${LIME_YELLOW}Installing ${BOLD}${YELLOW}ripgrep" 0 $(( ${#LIME_YELLOW} + ${#BOLD} + ${#YELLOW} )) "SUCCEED"
-        else
-            print_wtabs 2 "${LIME_YELLOW}Installing ${BLUE}ripgrep" 1 $(( ${#LIME_YELLOW} + ${#BLUE} )) "FAILED"
-        fi
-    fi
-else
-    printf "${BOLD}${YELLOW}ripgrep ${NORMAL}${YELLOW}found.\n"
-    printf "${NORMAL}"
-fi
+install_and_config "" rg ripgrep
 
 echo $SEPERATED_BAR
 
 ################################################################
 # Install 'fd'
-install_and_config "" fd
+install_and_config "" fd fd
 
 echo $SEPERATED_BAR
 
 ################################################################
 # Install 'bat'
-install_and_config "" bat
+install_and_config "" bat bat
 
 echo $SEPERATED_BAR
 
 ################################################################
 # Install 'jq'
-install_and_config "" jq
+install_and_config "" jq jq
 
 echo $SEPERATED_BAR
 
 ################################################################
-# Install 'sysstat'
-if ! command -v "mpstat" > /dev/null 2>&1; then
-    printf "${BOLD}${YELLOW}sysstat ${NORMAL}${YELLOW}is not installed => "
-    printf "${NORMAL}"
-
-    ask_for_confirmation "${YELLOW}Would you like to install ${BOLD}${YELLOW}ripgrep ${NORMAL}${YELLOW}now?" "y" "yes" "n" "no"
-
-    if [ $? -eq 0 ]; then
-        package_install "mpstat"
-
-        if [ $? -eq 0 ]; then
-            print_wtabs 2 "${LIME_YELLOW}Installing ${BOLD}${YELLOW}sysstat" 0 $(( ${#LIME_YELLOW} + ${#BOLD} + ${#YELLOW} )) "SUCCEED"
-        else
-            print_wtabs 2 "${LIME_YELLOW}Installing ${BLUE}sysstat" 1 $(( ${#LIME_YELLOW} + ${#BLUE} )) "FAILED"
-        fi
-    fi
-else
-    printf "${BOLD}${YELLOW}sysstat ${NORMAL}${YELLOW}found.\n"
-    printf "${NORMAL}"
-fi
+# # Install 'sysstat'
+install_and_config "" mpstat sysstat
 
 echo $SEPERATED_BAR
 
 ################################################################
 # Install and configure 'tmux'
-install_and_config terminal tmux .tmux.conf
+install_and_config "terminal" tmux tmux .tmux.conf
 
 echo $SEPERATED_BAR
 
 ################################################################
 # Install and configure 'vim/neovim' editor
-# install_and_config "" nvim
-# ln -fs ${HOME}/.config/nvim/configs/02.settings.vim ${HOME}/.vimrc
+install_and_config "" nvim neovim
 
 echo $SEPERATED_BAR
 
 ################################################################
 # Install and configure 'bash' shell
-install_and_config shell bash .bash_profile .bashrc
+install_and_config "shell" bash bash .bash_profile .bashrc
 
 echo $SEPERATED_BAR
 
 ################################################################
 # Install and configure 'zsh' shell
-install_and_config shell zsh .zprofile .zshrc .dir_colors
+install_and_config "shell" zsh zsh .zprofile .zshrc .dir_colors
 if [ $? -eq 0 ]; then
     $MAIN_DIR/post_install_zsh.sh
 fi
@@ -188,7 +151,7 @@ if command -v firefox > /dev/null 2>&1; then
             print_wtabs 1 "${BOLD}${BLUE}GTK_USE_PORTAL=1 ${NORMAL}${GREEN}already inside ${BLUE}${FIREFOX_SCRIPT}" 
         fi
     else
-        print_wtabs 1 "${RED}Cannot find ${BOLD}${FIREFOX_SCRIPT}." 0 $(( ${#RED} + ${#BOLD} )) "FAILED"
+        print_wtabs 1 "${RED}Cannot find ${BOLD}${FIREFOX_SCRIPT}." 1 $(( ${#RED} + ${#BOLD} )) "FAILED"
     fi
 
     printf "${NORMAL}"
@@ -203,5 +166,5 @@ echo $SEPERATED_BAR
 #---------------------------------------- END ----------------------------------------
 printf "${BOLD}${GREEN}COMPLETED dotfiles installation.\n"
 printf "${NORMAL}"
-printf "${BOLD}${LIME_YELLOW}You should check for any packages/configs ${RED}FAILED${LIME_YELLOW} and you might need to manually install it again.\n"
+printf "${BOLD}${LIME_YELLOW}You should check for any packages/configs ${RED}FAILED${LIME_YELLOW} and then you might need to manually install it again.\n"
 printf "${NORMAL}"
